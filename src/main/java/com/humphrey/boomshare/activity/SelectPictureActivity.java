ee711674ff2c65c66bd17b416f4bdb6a348ea37d
Humphrey_Zeng <humphrey_zeng@163.com>
2016-03-26 15:03:10 +0800

    添加nineoldandroid动画库前

--- src/main/java/com/humphrey/boomshare/activity/SelectPictureActivity.java
+++ src/main/java/com/humphrey/boomshare/activity/SelectPictureActivity.java
@@ -2,14 +2,30 @@ package com.humphrey.boomshare.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import com.humphrey.boomshare.R;
+import com.humphrey.boomshare.adapter.ChildAdapter;
+
+import java.util.List;
 
 public class SelectPictureActivity extends Activity {
+
+    private GridView mGridView;
+    private List<String> list;
+    private ChildAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_picture);
+
+        mGridView = (GridView) findViewById(R.id.gv_select_photo);
+        list = getIntent().getStringArrayListExtra("data");
+
+        adapter = new ChildAdapter(this, list, mGridView);
+        mGridView.setAdapter(adapter);
     }
+
+
 }
